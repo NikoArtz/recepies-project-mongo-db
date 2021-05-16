23755f7226fe1ca57253d373552b891795ffbfde
nikoartz <nikitaartsiomchyk@gmail.com>
2021-05-16 14:14:17 +0200

    Image rendering fix for reactive response

--- src/main/java/com/web/recipes/controllers/ImageController.java
+++ src/main/java/com/web/recipes/controllers/ImageController.java
@@ -3,6 +3,11 @@ package com.web.recipes.controllers;
 import com.web.recipes.commands.RecipeCommand;
 import com.web.recipes.services.ImageService;
 import com.web.recipes.services.RecipeService;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +15,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * @author martsiomchyk
@@ -43,21 +47,21 @@ public class ImageController {
         imageService.saveImageFile(id, file).share().block();
         return "redirect:/recipe/" + id + "/show";
     }
-//
-//    @GetMapping("recipe/{id}/recipe-image")
-//    public Mono<Void> renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
-//        RecipeCommand recipeCommand = recipeService.findCommandById(id).share().block();
-//        if (recipeCommand.getImage() != null) {
-//            byte[] byteArray = new byte[recipeCommand.getImage().length];
-//            int i = 0;
-//            for (Byte wrapperByte : recipeCommand.getImage()) {
-//                byteArray[i++] = wrapperByte;
-//            }
-//            response.setContentType("image/jpeg");
-//            try (InputStream is = new ByteArrayInputStream(byteArray)) {
-//                IOUtils.copy(is, response.getOutputStream());
-//            }
-//        }
-//        return Mono.empty();
-//    }
+
+    @GetMapping("recipe/{id}/recipe-image")
+    public Mono<Void> renderImageFromDB(@PathVariable String id, ServerHttpResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(id).share().block();
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+            for (Byte wrappedByte : recipeCommand.getImage()) {
+                byteArray[i++] = wrappedByte;
+            }
+            response.getHeaders().setContentType(MediaType.IMAGE_JPEG);
+            response.setStatusCode(HttpStatus.OK);
+            DataBuffer buffer = new DefaultDataBufferFactory().wrap(byteArray);
+            return response.writeWith(Flux.just(buffer));
+        }
+        return Mono.empty();
+    }
 }
