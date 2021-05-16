20f3e2447fe8cbb2581dab4385c8e551ab51443a
nikoartz <nikitaartsiomchyk@gmail.com>
2021-05-16 12:36:58 +0200

    Reactive exception handling

--- src/main/java/com/web/recipes/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/web/recipes/controllers/ControllerExceptionHandler.java
@@ -3,8 +3,12 @@ package com.web.recipes.controllers;
 import com.web.recipes.exceptions.BadRequestException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.thymeleaf.exceptions.TemplateInputException;
 
 
 /**
@@ -14,25 +18,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-//    @ExceptionHandler(BadRequestException.class)
-//    public ModelAndView handleBadRequestException(Exception exception, HttpServletResponse response) {
-//        log.error(exception.getClass() + exception.getMessage());
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("bad-request");
-//
-//        HttpStatus responseStatus;
-//        String exceptionMessage;
-//        if (exception instanceof BadRequestException) {
-//            BadRequestException badRequestException = (BadRequestException) exception;
-//            responseStatus = badRequestException.getStatus();
-//            exceptionMessage = badRequestException.getReason();
-//        } else {
-//            responseStatus = HttpStatus.BAD_REQUEST;
-//            exceptionMessage = exception.getMessage();
-//        }
-//        response.setStatus(responseStatus.value());
-//        modelAndView.addObject("status", responseStatus.toString());
-//        modelAndView.addObject("exception", exceptionMessage);
-//        return modelAndView;
-//    }
+    @ExceptionHandler({BadRequestException.class, TemplateInputException.class})
+    public String handleBadRequestException(Exception exception, Model model, ServerHttpResponse response) {
+       log.error(exception.getClass() + exception.getMessage());
+       
+        HttpStatus responseStatus;
+        String exceptionMessage;
+        if (exception instanceof BadRequestException) {
+            BadRequestException badRequestException = (BadRequestException) exception;
+            responseStatus = badRequestException.getStatus();
+            exceptionMessage = badRequestException.getReason();
+        } else {
+            responseStatus = HttpStatus.NOT_FOUND;
+            exceptionMessage = exception.getMessage();
+        }
+        response.setStatusCode(responseStatus);
+        model.addAttribute("status", responseStatus.toString());
+        model.addAttribute("exception", exceptionMessage);
+        return "bad-request";
+    }
 }
