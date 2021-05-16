f01a365919634a900595f05c38f0f74725f45bb2
nikoartz <nikitaartsiomchyk@gmail.com>
2021-05-16 12:57:32 +0200

    Json API

---
+++ src/main/java/com/web/recipes/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.web.recipes.config;
+
+import com.web.recipes.domain.Recipe;
+import com.web.recipes.services.RecipeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+
+/**
+ * @author martsiomchyk
+ */
+
+@Configuration
+public class WebConfig {
+    @Bean
+    RouterFunction<?> routes(RecipeService recipeService) {
+        return RouterFunctions.route(GET("/api/recipes"), serverRequest -> ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(recipeService.getRecipes(), Recipe.class));
+    }
+}
